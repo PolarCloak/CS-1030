05851d731390819f641ce971583d7dcf1a0c76f9
David Guest <davidgguest2@gmail.com>
2022-02-01 12:17:27 -0600

    2.6
    sum of digits, I made sure that it was possible to use numbers far past 1000 as it would be bad coding to only allow a finite list

--- Guest_David_Assignment_2/Guest_David_Assignment_2_6.py
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_6.py
@@ -5,18 +5,28 @@ David Guest
 Assignment #2.6
 Due Date: 2/11/2022
 
-
+Sum the digits in an integer
 """
 
 
-#
-def method():
-    return
+# Takes a user-defined int and runs through each digit, adding to a sum. returns the sum.
+def convert_and_add(user_input):
+    # initialize values used in loop
+    sum_of_digits = 0
+    value1 = user_input
+    # while the value still has at least 2 or more digits
+    while (value1 // 10) > 0:
+        sum_of_digits += value1 % 10
+        value1 = value1 // 10
+    # add the last digit to the sum
+    sum_of_digits += value1
+    return sum_of_digits
 
 
 # run my methods starting here
 def main(args):
-    method()
+    user_input = eval(input("Enter any integer (should work well past 1000): "))
+    print("The sum of the digits is {0}".format(convert_and_add(user_input)))
 
 
 main(0)
