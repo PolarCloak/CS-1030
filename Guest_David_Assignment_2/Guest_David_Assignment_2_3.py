b9480b2d29709cbc6c23e4a508324b5d5995d48d
David Guest <davidgguest2@gmail.com>
2022-02-01 11:15:50 -0600

    start
    added basic info and methods to all new files

---
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_3.py
@@ -0,0 +1,22 @@
+"""
+CS10300-20766
+ID#700714467
+David Guest
+Assignment #2.3
+Due Date: 2/11/2022
+
+
+"""
+
+
+#
+def method():
+    return
+
+
+# run my methods starting here
+def main(args):
+    method()
+
+
+main(0)
