eb0c4a14f17807e1430da6d39955444e24063125
David Guest <davidgguest2@gmail.com>
2022-02-01 11:50:20 -0600

    2.4
    another simple conversion from lbs to kgs

--- Guest_David_Assignment_2/Guest_David_Assignment_2_4.py
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_4.py
@@ -5,18 +5,19 @@ David Guest
 Assignment #2.4
 Due Date: 2/11/2022
 
-
+Convert pounds into kilograms
 """
 
 
-#
-def method():
-    return
+# convert value in pounds to kilograms
+def lbs_to_kgs(lbs):
+    return lbs * 0.454
 
 
 # run my methods starting here
 def main(args):
-    method()
+    lbs = float(input("Enter a value in pounds: "))
+    print("{0} pounds is {1} kilograms ".format(lbs, lbs_to_kgs(lbs).__round__(3)))
 
 
 main(0)
