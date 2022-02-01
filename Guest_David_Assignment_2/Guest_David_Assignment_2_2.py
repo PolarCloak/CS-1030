b16e02c3975ef020ae8bd0263114f50d387855c6
David Guest <davidgguest2@gmail.com>
2022-02-01 11:42:23 -0600

    2.2
    Completed 2.2, made sue to make use of multi-input assignment and round to values shown in the textbook

--- Guest_David_Assignment_2/Guest_David_Assignment_2_2.py
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_2.py
@@ -7,16 +7,27 @@ Due Date: 2/11/2022
 
 Compute the volume of a cylinder
 """
+import math
 
 
-#
-def method():
-    return
+# the math to convert a radius to an area
+def compute_area(radius):
+    area = radius * radius * math.pi
+    print("The area is {0}".format(area.__round__(4)))
+    return area
+
+
+# method to compute the volume of a cylinder
+def compute_volume(radius, length):
+    area = compute_area(radius)
+    volume = area * length
+    return volume
 
 
 # run my methods starting here
 def main(args):
-    method()
+    radius, length = eval(input("Insert values: {radius}, {length} : "))
+    print("The volume is {0}.".format(compute_volume(radius, length).__round__(1)))
 
 
 main(0)
