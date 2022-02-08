e54b0c81698f1c5f9b64600c1abf414c7b50e37f
David Guest <davidgguest2@gmail.com>
2022-02-08 11:37:41 -0600

    Finished 2.7 and 2.8
    for 2.7, I wanted to save myself some tedious  writing on the methods and decided to use a global variable to store the results used in the print statement. for 2.8, very simple math calculation

--- Guest_David_Assignment_2/Guest_David_Assignment_2_7.py
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_7.py
@@ -5,29 +5,37 @@ David Guest
 Assignment #2.7
 Due Date: 2/11/2022
 
-
+This program converts an input of minutes into a total number of years and the remaining
+days towards the next year
 """
+# variables used to store the total years and leftover days after calculation
+leftoverDays = 0
+totalYears = 0
 
 
-#
+# converts number of days to years, and then stores the leftover days in a global variable
 def days_to_years(days):
+    global totalYears
+    global leftoverDays
+    totalYears = days / 365
+    leftoverDays = days % 365
+    return totalYears
 
-    return
 
-
-#
+# converts number of minutes to number of days
 def minutes_to_days(minutes):
-    hours = minutes/60
-    days = hours/24
+    hours = minutes / 60
+    days = hours / 24
     return days
 
 
 # run my methods starting here
 def main(args):
     minutes = eval(input("Enter the number of minutes: "))
-    print("{0} degrees Celsius is {1} degrees Fahrenheit.".format(minutes, convert_to_f(celsius).__format__(".2f")))
-
-1000000000 minutes is approximately 1902 years and 214 days
+    days_to_years(minutes_to_days(minutes))
+    print(
+        "{0} minutes is approximately {1} years and {2} days.".format(int(minutes), int(totalYears),
+                                                                      int(leftoverDays)))
 
 
 main(0)
--- Guest_David_Assignment_2/Guest_David_Assignment_2_8.py
+++ Guest_David_Assignment_2/Guest_David_Assignment_2_8.py
@@ -5,18 +5,23 @@ David Guest
 Assignment #2.8
 Due Date: 2/11/2022
 
-
+Converts an input value of mass, starting temp, and final temp and returns the joules needed to  heat it up
 """
 
 
-#
-def method():
-    return
+# takes in mass of water used, starting temp and final temp to calculate the joules needed to heat it
+def get_joules(mass, start_temp, end_temp):
+    joules = mass * (end_temp - start_temp) * 4184
+    return joules
 
 
 # run my methods starting here
 def main(args):
-    method()
+    mass = eval(input("Enter the amount of water in kilograms: "))
+    start_temp = eval(input("Enter the initial temperature: "))
+    end_temp = eval(input("Enter the final temperature: "))
+    joules = get_joules(mass, start_temp, end_temp)
+    print("The energy needed is {0} joules.".format(joules.__round__("0.1f")))
 
 
 main(0)
