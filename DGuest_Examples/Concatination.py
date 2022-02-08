59f3e6ee8e72c569478d3f84ad42d7a8f3a2d43a
David Guest <davidgguest2@gmail.com>
2022-02-08 12:08:17 -0600

    classwork

---
+++ DGuest_Examples/Concatination.py
@@ -0,0 +1,13 @@
+"""
+CS10300-20766
+ID#700714467
+David Guest
+Class Work
+Due Date: N/A
+
+Determining the area of a circle.
+"""
+
+first_name, last_name = input("Input first name, last name: ").split(",")
+
+print("Welcome, {0} {1}, to Sword Art Online.".format(first_name, last_name))
