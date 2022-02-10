6988fe8dd86d9ad6eea1e12a3326907ff6493fbd
David Guest <davidgguest2@gmail.com>
2022-02-10 12:38:34 -0600

    finished 3.4

--- Guest_David_Assignment_3/Guest_David_Assignment_3_4.py
+++ Guest_David_Assignment_3/Guest_David_Assignment_3_4.py
@@ -7,16 +7,20 @@ Due Date: 2/25/2022
 
 Geometry: Area of a pentagon
 """
+import math
 
 
-#
-def method():
-    return
+# calculates the area of a pentagon using the length of a side
+def calculate_pentagon(side_length):
+    area = (5 * (side_length ** 2)) / (4 * (math.tan(math.pi / 5)))
+    return area
 
 
 # run my methods starting here
 def main(args):
-    method()
+    side_in = eval(input("Enter the side: "))
+    area = calculate_pentagon(side_in)
+    print(f"The area of the pentagon is {area}")
 
 
 main(0)
