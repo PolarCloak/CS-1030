81c98460b41b20e807e918158b66c039d880dcbc
David Guest <davidgguest2@gmail.com>
2022-02-10 11:57:26 -0600

    created all the assignment files

---
+++ Guest_David_Assignment_3/Guest_David_Assignment_3_2.py
@@ -0,0 +1,22 @@
+"""
+CS10300-20766
+ID#700714467
+David Guest
+Assignment #3.2
+Due Date: 2/25/2022
+
+
+"""
+
+
+#
+def method():
+    return
+
+
+# run my methods starting here
+def main(args):
+    method()
+
+
+main(0)
