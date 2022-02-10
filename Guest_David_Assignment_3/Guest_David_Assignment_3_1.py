f9e62f38ea586140382ad114300f69a12fa8da96
David Guest <davidgguest2@gmail.com>
2022-02-10 12:30:58 -0600

    assignment 3.1 finished

--- Guest_David_Assignment_3/Guest_David_Assignment_3_1.py
+++ Guest_David_Assignment_3/Guest_David_Assignment_3_1.py
@@ -7,16 +7,26 @@ Due Date: 2/25/2022
 
 Geometry: Area of a Pentagon
 """
+import math
 
 
-#
-def method():
-    return
+# determines the area of a pentagon by using the length of a side
+def get_area(length_of_side):
+    area = (3 * math.sqrt(3)) / 2 * length_of_side ** 2
+    return area
+
+
+# determines the length of the side of a pentagon by using the length from the center
+def get_side(length_from_center):
+    side = (2 * length_from_center) * math.sin(math.pi / 5)
+    return side
 
 
 # run my methods starting here
 def main(args):
-    method()
+    length_in = eval(input(f"Please input a value for the length from the center of the pentagon: "))
+    result = get_area(get_side(length_in))
+    print(f"The area of the pentagon is {result:.2f}")
 
 
 main(0)
