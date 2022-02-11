fe57d597489dcf4c829e68e4c3f86cb9e0e7afbf
David Guest <davidgguest2@gmail.com>
2022-02-11 10:25:34 -0600

    finished 3.2 before class, tested

--- Guest_David_Assignment_3/Guest_David_Assignment_3_2.py
+++ Guest_David_Assignment_3/Guest_David_Assignment_3_2.py
@@ -8,16 +8,29 @@ Due Date: 2/25/2022
 Geometry: Great circle distance
 The great circle distance is the distance between two points on the surface of a sphere.
 """
+import math
 
 
-#
-def method():
-    return
+# calculates the distance between the two radian points.
+def calculate_distance(radian1_x, radian1_y, radian2_x, radian2_y):
+    radius = 6371.01
+    distance = radius * math.acos(
+        math.sin(radian1_x) * math.sin(radian2_x) + math.cos(radian1_x) * math.cos(radian2_x) * math.cos(
+            radian1_y - radian2_y))
+    return distance
+
+
+# just converts a degree into a radian
+def rad(degrees):
+    return math.radians(degrees)
 
 
 # run my methods starting here
 def main(args):
-    method()
+    point1_x, point1_y = eval(input(f"Enter point 1 (latitude and longitude) in degrees: "))
+    point2_x, point2_y = eval(input(f"Enter point 2 (latitude and longitude) in degrees: "))
+    distance = calculate_distance(rad(point1_x), rad(point1_y), rad(point2_x), rad(point2_y))
+    print(f"The distance between the two points is {distance} km")
 
 
 main(0)
