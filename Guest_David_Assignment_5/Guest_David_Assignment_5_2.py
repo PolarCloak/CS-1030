8aed6aa8ae901f832e7e6efd68346165fa9b2ac3
David Guest <davidgguest2@gmail.com>
2022-03-22 12:25:10 -0500

    finished 5.3

--- Guest_David_Assignment_5/Guest_David_Assignment_5_2.py
+++ Guest_David_Assignment_5/Guest_David_Assignment_5_2.py
@@ -7,10 +7,51 @@ Due Date: 4/8/22
 
 Repeat additions
 """
+import random
+import time
+
+# variables used to modify the quiz
+QUESTION_COUNT = 5
+MINIMUM = 1
+MAXIMUM = 15
+
+
+# variables where answer results are stored
+correct = 0
+
+
+# this is the quiz function that runs {question} number of times using max and min
+def quiz(questions, minimum, maximum):
+    global correct
+    for q in range(1, questions + 1, 1):
+        print(f"Question {q}:")
+        int1 = random.randrange(minimum, maximum, 1)
+        int2 = random.randrange(minimum, maximum, 1)
+        guess = eval(input(f"What is the sum: {int1} + {int2} ?\n"))
+        answer = int1 + int2
+        # correct answer
+        if guess == answer:
+            correct += 1
+            print(f"\nCorrect!\n")
+        else:
+            print(f"\nWrong...\n")
+
+
+# displays the results of your quiz
+def displayResults(timeTaken):
+    print(f"Your quiz took {timeTaken:.2f} seconds.")
+    print(f"You were able to answer {correct} / {QUESTION_COUNT} correctly\n")
+    percent = correct / QUESTION_COUNT
+    print(f"Your total grade is: {percent:.2%}")
 
 
 # run my methods starting here
 def main():
+    startTime = time.time()
+    quiz(QUESTION_COUNT, MINIMUM, MAXIMUM)
+    endTime = time.time()
+    timeTaken = endTime - startTime
+    displayResults(timeTaken)
     return
 
 
--- Guest_David_Assignment_5/Guest_David_Assignment_5_3.py
+++ Guest_David_Assignment_5/Guest_David_Assignment_5_3.py
@@ -7,10 +7,25 @@ Due Date: 4/8/22
 
 Conversion from kilograms to pounds
 """
+# static variables
+KG_TO_LB = 2.2
+
+
+# prints the top part of the table
+def tableTop():
+    print(f"Kilograms       Pounds")
+
+
+# displays the table starting from 1 to 199 using only the odds
+def tableLoop():
+    for kgs in range(1, 200, 2):
+        print(f"{kgs}               {kgs * KG_TO_LB:.1f}")
 
 
 # run my methods starting here
 def main():
+    tableTop()
+    tableLoop()
     return
 
 
