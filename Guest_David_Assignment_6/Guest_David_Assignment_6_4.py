2c30c148322e5b73b20b5c240c09083d9c371c39
David Guest <davidgguest2@gmail.com>
2022-04-07 11:55:48 -0500

    Finished 6.4 and 6.5

--- Guest_David_Assignment_6/Guest_David_Assignment_6_4.py
+++ Guest_David_Assignment_6/Guest_David_Assignment_6_4.py
@@ -7,22 +7,23 @@ Due Date: 4/22/22
 
 Display an integer reversed
 """
-# imports
 
 
-# static variables
-
-
-# global variables
-
-
-#
-def method1():
-    return
+# takes a number variable, and returns the reverse of that number.
+def reverse(number):
+    numString = str(number)
+    chars = list(numString)
+    # python has a built-in function for reversing a list.
+    chars.reverse()
+    # and then for putting the chars back together in a variable
+    reversedString = "".join(chars)
+    return reversedString
 
 
 # run my methods starting here
 def main():
+    given = input(f"Type anything and I will display it in reverse: ")
+    print(f"Reversed: {reverse(given)}")
     return
 
 
--- Guest_David_Assignment_6/Guest_David_Assignment_6_5.py
+++ Guest_David_Assignment_6/Guest_David_Assignment_6_5.py
@@ -5,24 +5,32 @@ David Guest
 Assignment #6.5
 Due Date: 4/22/22
 
-Sort three numbers
+Sort three numbers (added a choice for ascending or descending)
 """
-# imports
-
-
-# static variables
-
-
 # global variables
+sortFlag = "a"
 
 
-#
-def method1():
-    return
+# takes in 3 values and returns them in an array sorted. Increasing order.
+def displaySortedNumbers(num1, num2, num3):
+    items = list()
+    items.append(num1)
+    items.append(num2)
+    items.append(num3)
+    # now that they are all in a list, built-in sorts are available
+    if sortFlag.lower() == "d" or sortFlag.lower() == "desc":
+        items.sort(reverse=True)
+    else:
+        items.sort()
+    return items
 
 
 # run my methods starting here
 def main():
+    global sortFlag
+    x, y, z = eval(input(f"Enter three numbers: "))
+    sortFlag = input(f"What order would you like it sorted? (A/D):")
+    print(f"The sorted numbers are: {displaySortedNumbers(x, y, z)}")
     return
 
 
